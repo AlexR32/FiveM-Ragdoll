b2c4217e8758127fae57f0cf1539062961d79f8a
Alex <58864658+AlexR32@users.noreply.github.com>
2020-09-09 14:18:45 +0300

    First release

---
+++ Ragdoll/Config.lua
@@ -0,0 +1,17 @@
+Config = {
+    Control = {
+        Toggle = 74, -- H
+        Forward = 32, -- W
+        Backward = 33, -- S
+        Left = 34, -- A
+        Right = 35, -- D
+        Up = 44, -- Q
+        Down = 48, -- Z
+        Increase = 45, -- R
+        Decrease = 23, -- F
+        Mode = 38 -- E
+    },
+    Debug = false, -- Prints coords to console
+    Speed = 100.0, -- Default speed (Default: 100.0)
+    Mode = 0 -- Default mode (Default: 0)
+}
---
+++ Ragdoll/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "bodacious"
+game "gta5"
+author "AlexR32"
+version "1.0.0"
+
+client_script "Config.lua"
+client_script "Client.lua"
+client_script "Controls.lua"
