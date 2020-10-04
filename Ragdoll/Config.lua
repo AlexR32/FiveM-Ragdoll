64db879b6df4dfe61f55d884f7638088bf4a8ed3
Alex <58864658+AlexR32@users.noreply.github.com>
2020-10-04 10:29:05 +0300

    Forgot to change value

--- Ragdoll/Config.lua
+++ Ragdoll/Config.lua
@@ -14,4 +14,4 @@ Control = {
 Debug = false -- Print coords to console (Default: false)
 Speed = 100.0 -- How fast the ragdoll will fly (Default: 100.0) (Speed: 0.0 - 200.0)
 Mode = 2 -- Default mode when logging into the server (Default: 2) (Mode: 1 - 2) (1 - Just a ragdoll 2 - ped tries to stay on his feet)
-ToggleControls = false -- Disable/Enable controls by default when logging into server (dont change value if server running, this will broke instructional buttons) (Default: true)
+ToggleControls = true -- Disable/Enable controls by default when logging into server (dont change value if server running, this will broke instructional buttons) (Default: true)
